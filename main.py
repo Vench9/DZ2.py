386cf0e3237f7c958a00ab3e6d418a91b8840b6c
Vench_97 <vnuk-vanya@mail.ru>
2021-10-13 15:02:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+school = {'1а':10, '2b':15, '2a':14, '3a':20, '4a':18, '4b':13, '5a':17, '5b':22, '6a':24, '6b':13}
+print(school ['3a'])
+school ['2b'] = 13
+school ['4a'] = 17
+school ['6b'] = 20
+print(school)
+school ['7a'] = '30'
+school ['9b'] = '22'
+print(school)
+del school ['6a']
+print(school)
